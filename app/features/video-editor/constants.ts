35014e8b187bab6e071af7a7a9f84c986485e2b5
Matt Pocock <mattpocockvoice@gmail.com>
2026-03-09 12:17:05 +0000

    Increased the audio boost

--- app/features/video-editor/constants.ts
+++ app/features/video-editor/constants.ts
@@ -2,4 +2,4 @@ export const FINAL_VIDEO_PADDING = 0.5 - 0.08;
 export const INSERTION_POINT_ID = "insertion-point";
 export const RECORDING_SESSION_PANELS_ID = "recording-session-panels";
 export const BEAT_DURATION = 0.4;
-export const PREVIEW_AUDIO_BOOST_DB = 10;
+export const PREVIEW_AUDIO_BOOST_DB = 14;
